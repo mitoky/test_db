981de4e77a7c940fa785123d13826f260f634314
nyaro <matthewandriamitantsoa@gmail.com>
2026-06-10 13:43:09 +0300

    Add files via upload
    niova ny fonction.php

--- fonction.php
+++ fonction.php
@@ -36,6 +36,11 @@ function get_dep(){
     $sql = "SELECT d.dept_no numero_departement,d.dept_name nom_departement,first_name Prenom,last_name Nom FROM departments d JOIN dept_manager dm ON d.dept_no = dm.dept_no JOIN employees e ON dm.emp_no = e.emp_no WHERE dm.to_date > CURDATE()";
     $result = mysqli_query(dbconnect(), $sql);
     return get_all_lines($sql);
-    }  
-
+}  
+function get_liste($id){
+    $sql = "SELECT d.dept_no numero_departement, d.dept_name nom_departement, e.emp_no id_employe, e.first_name Prenom, e.last_name Nom from dept_emp de join employees e on de.emp_no = e.emp_no JOIN departments d ON de.dept_no = d.dept_no where de.dept_no= '%s'";
+    $sql = sprintf($sql, $id);
+    $result = mysqli_query(dbconnect(), $sql);
+    return get_all_lines($sql);
+}  
 ?>
